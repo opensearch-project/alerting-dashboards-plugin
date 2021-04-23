b0aaf21df9033bf12647733be9d42d9dff0d39ea
Ashish Agrawal <ashisagr@amazon.com>
2021-04-23 11:51:53 -0700

    fix cypress tests

--- cypress/integration/monitor_spec.js
+++ cypress/integration/monitor_spec.js
@@ -41,7 +41,7 @@ describe('Monitors', () => {
     localStorage.setItem('home:welcome:show', 'false');
 
     // Visit Alerting OpenSearch Dashboards
-    cy.visit(`${Cypress.env('kibana')}/app/${PLUGIN_NAME}#/monitors`);
+    cy.visit(`${Cypress.env('opensearch_dashboards')}/app/${PLUGIN_NAME}#/monitors`);
 
     // Common text to wait for to confirm page loaded, give up to 20 seconds for initial load
     cy.contains('Create monitor', { timeout: 20000 });
