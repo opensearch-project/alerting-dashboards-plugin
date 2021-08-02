3b12833b9dde332457a351141b950a678eeb75a2
Annie Lee <leeyun@amazon.com>
2021-08-02 11:13:19 -0700

    Update VisualGraph.js

--- public/pages/CreateMonitor/components/VisualGraph/VisualGraph.js
+++ public/pages/CreateMonitor/components/VisualGraph/VisualGraph.js
@@ -38,8 +38,10 @@ import {
   getMarkData,
   getAggregationTitle,
   getRectData,
+  getCustomAggregationTitle,
 } from './utils/helpers';
 import BarSeries from 'react-vis/es/plot/series/bar-series';
+import { MONITOR_TYPE } from '../../../../utils/constants';
 
 export default class VisualGraph extends Component {
   static defaultProps = { annotation: false };
@@ -67,8 +69,63 @@ export default class VisualGraph extends Component {
     const rectData = getRectData(data);
     const aggregationTitle = getAggregationTitle(values);
 
-    // Debug use
-    console.log('Printing out markData: ' + JSON.stringify(markData));
+    return (
+      <div>
+        <FlexibleXYPlot
+          height={400}
+          xType="time"
+          margin={{ top: 20, right: 20, bottom: 70, left: leftPadding }}
+          xDomain={xDomain}
+          yDomain={yDomain}
+          onMouseLeave={this.resetHint}
+        >
+          <XAxis title={xTitle} />
+          <XAxis
+            title={aggregationTitle}
+            position="middle"
+            orientation="top"
+            tickTotal={0}
+            top={-25}
+            style={{ strokeWidth: '0px' }}
+          />
+          <YAxis title={yTitle} tickFormat={formatYAxisTick} />
+          {/*TODO: Convert to LineMarkSeries with individual data (e.g. "key1Data", "key2Data"*/}
+          <LineSeries data={data} style={LINE_STYLES} />
+          <MarkSeries data={markData} sizeRange={SIZE_RANGE} onNearestX={this.onNearestX} />
+          {/*TODO: Add each group by data with a bar series graph using different colors*/}
+          {/*<VerticalBarSeries data={rectData} />*/}
+          {annotation && <LineSeries data={annotations} style={ANNOTATION_STYLES} />}
+          {hint && (
+            <Hint value={hint}>
+              <div style={HINT_STYLES}>({hint.y.toLocaleString()})</div>
+            </Hint>
+          )}
+        </FlexibleXYPlot>
+        {/*<FlexibleXYPlot*/}
+        {/*  height={400}*/}
+        {/*  xType="time"*/}
+        {/*  margin={{ top: 20, right: 20, bottom: 70, left: leftPadding }}*/}
+        {/*  xDomain={xDomain}*/}
+        {/*  yDomain={yDomain}*/}
+        {/*  onMouseLeave={this.resetHint}*/}
+        {/*></FlexibleXYPlot>*/}
+      </div>
+    );
+  };
+
+  renderAggregationXYPlot = (data) => {
+    const { annotation, thresholdValue, values, fieldName, aggregationType } = this.props;
+    const { hint } = this.state;
+    const xDomain = getXDomain(data);
+    const yDomain = getYDomain(data);
+    const annotations = getAnnotationData(xDomain, yDomain, thresholdValue);
+    const xTitle = values.timeField;
+    const yTitle = getYTitle(values);
+    const leftPadding = getLeftPadding(yDomain);
+    const markData = getMarkData(data);
+    const rectData = getRectData(data);
+    const aggregationTitle = getCustomAggregationTitle(values, fieldName, aggregationType);
+    // Debug use console.log('Printing out markData: ' + JSON.stringify(markData));
     console.log('Printing out data: ' + JSON.stringify(data));
     return (
       <div>
@@ -110,24 +167,6 @@ export default class VisualGraph extends Component {
         {/*  yDomain={yDomain}*/}
         {/*  onMouseLeave={this.resetHint}*/}
         {/*></FlexibleXYPlot>*/}
-        // Debug use console.log('Printing out markData: ' + JSON.stringify(markData));
-        console.log('Printing out data: ' + JSON.stringify(data));
-        <FlexibleXYPlot
-          height={400}
-          xType="time"
-          margin={{ top: 20, right: 20, bottom: 70, left: leftPadding }}
-          xDomain={xDomain}
-          yDomain={yDomain}
-          onMouseLeave={this.resetHint}
-        ></FlexibleXYPlot>
-        {/*<FlexibleXYPlot*/}
-        {/*  height={400}*/}
-        {/*  xType="time"*/}
-        {/*  margin={{ top: 20, right: 20, bottom: 70, left: leftPadding }}*/}
-        {/*  xDomain={xDomain}*/}
-        {/*  yDomain={yDomain}*/}
-        {/*  onMouseLeave={this.resetHint}*/}
-        {/*></FlexibleXYPlot>*/}
       </div>
     );
   };
@@ -141,15 +180,26 @@ export default class VisualGraph extends Component {
   );
 
   render() {
-    const { response, fieldName, values } = this.props;
+    const { response, fieldName, values, aggregationType } = this.props;
     const monitorType = values.monitor_type;
-    const data = getDataFromResponse(response, fieldName, monitorType);
+    const isTraditionalMonitor = monitorType === MONITOR_TYPE.TRADITIONAL;
+    const aggTypeFieldName = `${aggregationType}_${fieldName}`;
 
     // Debug use
     console.log('values: ' + JSON.stringify(values));
+    //TODO: Pass in the groupBy field to the following method in order to sort data
+    const data = getDataFromResponse(response, aggTypeFieldName, monitorType);
+    // Show empty graph view when data is empty or aggregation monitor does not have group by defined.
+    const showEmpty =
+      !data.length || (monitorType == MONITOR_TYPE.AGGREGATION && !values.groupBy.length);
+
     return (
       <div style={{ padding: '20px', border: '1px solid #D9D9D9', borderRadius: '5px' }}>
-        {data.length ? this.renderXYPlot(data) : this.renderEmptyData()}
+        {showEmpty
+          ? this.renderEmptyData()
+          : isTraditionalMonitor
+          ? this.renderXYPlot(data)
+          : this.renderAggregationXYPlot(data)}
       </div>
     );
   }
