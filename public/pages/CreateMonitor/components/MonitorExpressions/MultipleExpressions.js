4d623e6536d4f3032cb1551607df9a10ffeeca44
Annie Lee <71157062+leeyun-amzn@users.noreply.github.com>
2021-08-11 14:48:09 -0700

    Update MultipleExpressions.js

--- public/pages/CreateMonitor/components/MonitorExpressions/MultipleExpressions.js
+++ public/pages/CreateMonitor/components/MonitorExpressions/MultipleExpressions.js
@@ -12,7 +12,11 @@
 import React, { Component } from 'react';
 import { EuiSpacer } from '@elastic/eui';
 
-import { ForExpression, OfExpression, WhereExpression } from './expressions';
+import {
+  ForExpression,
+  OfExpression,
+  WhereExpression,
+} from './expressions';
 import MetricExpression from './expressions/MetricExpression';
 
 export const DEFAULT_CLOSED_STATES = {
