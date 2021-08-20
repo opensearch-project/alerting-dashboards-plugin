b5185e605e0152e56e4a6cd9741988fd3481ae43
AWSHurneyt <79280347+AWSHurneyt@users.noreply.github.com>
2021-08-19 20:01:44 -0700

    Implemented test message toast. Fixed alerts dashboard severity display bug. (#52)
    * Refactored MonitorType such that the AD definition option cannot be selected for aggregation monitors. Also implemented logic to clear the form when changing from an unsupported definition option to aggregation monitor type. Reimplemented the query response display in the trigger panel when defining an AD trigger condition using the query response option. Also made minor edits to various other classes to align UX with mocks.
    
    * Refactored actions panel to align with new mocks. Disabled throttling when per execution is selected in the action configuration.
    
    * Refactored the visual editor for bucket-level triggers to align with new mocks.
    
    * Refactored the visual and response editor options for query-level AD triggers to align with new mocks. Refactored form reset logic to remove trigger definitions from the form when changing the monitor type.
    
    * Implemented logic to set bucket-level monitor search type to a default value when changing from a query-level monitor with an unsupported search type.
    
    * Refactored monitor details panel on the monitor creation/edit page to align with mocks.
    
    * Implemented flyout side panel for the alerts dashboard.
    
    * Refactored the popover used by query-level visual trigger editor to dropdown menus. Refactored actions panel to hide throttling options when per execution is selected. Set a default throttling value for bucket-level actions. Refactored margins and padding in various classes to align with mocks
    
    * Refactored query trigger definition components so code editor box sizes align with mocks. Add a prompt to select a detector for anomaly detection monitors when.
    
    * Refactored the alert dashboard flyout to remove the close button in the top-right corner. Moved the detector link on the monitor details page for anomaly detection monitors from under the monitor name to the overview table.
    
    * Fixed a bug that was causing the alerts dashboard flyout to display a severity 1 level higher than the actual level. Moved the log statement to the default case of getOverviewStats::getMonitorLevelType. Implemented a toast indicating when a test message is sent to a destination.
    
    Signed-off-by: Annie Lee <leeyun@amazon.com>

--- public/components/Flyout/flyouts/alertsDashboard.js
+++ public/components/Flyout/flyouts/alertsDashboard.js
@@ -61,6 +61,10 @@ const getBucketLevelGraphConditions = (trigger) => {
   });
 };
 
+const getSeverityText = (severity) => {
+  return _.get(_.find(SEVERITY_OPTIONS, { value: severity }), 'text');
+};
+
 const getBucketLevelGraphFilter = (trigger) => {
   const compositeAggFilter = _.get(trigger, 'condition.composite_agg_filter');
   if (_.isEmpty(compositeAggFilter)) return DEFAULT_WHERE_EXPRESSION_TEXT;
@@ -156,7 +160,7 @@ const alertsDashboard = (payload) => {
           <EuiFlexItem>
             <EuiText size={'m'}>
               <strong>Severity</strong>
-              <p>{SEVERITY_OPTIONS[severity].text || severity}</p>
+              <p>{getSeverityText(severity) || severity || '-'}</p>
             </EuiText>
           </EuiFlexItem>
         </EuiFlexGroup>
@@ -196,7 +200,7 @@ const alertsDashboard = (payload) => {
         <EuiFlexGroup>
           <EuiFlexItem>
             <EuiText size={'m'}>
-              <strong>Conditions</strong>
+              <strong>{_.isArray(condition) ? 'Conditions' : 'Condition'}</strong>
               {loadingMonitors ? (
                 'Loading conditions...'
               ) : _.isArray(condition) ? (
