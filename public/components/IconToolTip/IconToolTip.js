559b479ba25feb54022b5420d304a6e794cf8768
Annie Lee <leeyun@amazon.com>
2021-09-09 15:04:49 -0700

    Add icon tooltip

---
+++ public/components/IconToolTip/IconToolTip.js
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { EuiIcon, EuiToolTip } from '@elastic/eui';
+
+const IconToolTip = ({ iconType, content, position = 'top' }) => (
+  <EuiToolTip position={position} content={content}>
+    <EuiIcon type={iconType} />
+  </EuiToolTip>
+);
+
+IconToolTip.propTypes = {
+  iconType: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  position: PropTypes.string,
+};
+
+export default IconToolTip;
