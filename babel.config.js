41ebe28d827e0cd872c0084d03fdb126b0226e1e
Annie Lee <leeyun@amazon.com>
2021-11-09 17:02:36 -0800

    Update babel config to let test pass

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 // babelrc doesn't respect NODE_PATH anymore but using require does.
 // Alternative to install them locally in node_modules
 module.exports = {
-  presets: [require('@babel/preset-env'), require('@babel/preset-react')],
+  presets: [
+    require('@babel/preset-env'),
+    require('@babel/preset-react'),
+    require('@babel/preset-typescript'),
+  ],
   plugins: [
     require('@babel/plugin-proposal-class-properties'),
     require('@babel/plugin-proposal-object-rest-spread'),
