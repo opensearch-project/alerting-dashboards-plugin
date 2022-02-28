d3dc41439922100c1b8895ce2c08b040f0280366
Annie Lee <leeyun@amazon.com>
2022-02-28 14:38:35 -0800

    Refactor error messages to add period
    Signed-off-by: Annie Lee <leeyun@amazon.com>

--- public/pages/CreateMonitor/components/MonitorTimeField/utils/validation.test.js
+++ public/pages/CreateMonitor/components/MonitorTimeField/utils/validation.test.js
@@ -11,14 +11,14 @@ describe('validation', () => {
   test('prompts user to select time field when no value is selected', () => {
     const dateFields = ['customer_birth_date', 'order_date', 'products.created_on'];
     const value = undefined;
-    expect(validateTimeField(dateFields)(value)).toBe('Select a time field');
+    expect(validateTimeField(dateFields)(value)).toBe('Select a time field.');
   });
 
   test('shows error message if the option is not in dateFields', () => {
     const dateFields = ['customer_birth_date', 'order_date', 'products.created_on'];
     const value = 'invalidValue';
     expect(validateTimeField(dateFields)(value)).toBe(
-      `The field "${value}" does not exist in the selected index mappings, please choose a new time field`
+      `The field "${value}" does not exist in the selected index mappings, please choose a new time field.`
     );
   });
 });
