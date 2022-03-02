33b2d49cc0227003f8d2b7cae03e9aa9519df1c1
AWSHurneyt <hurneyt@amazon.com>
2022-03-02 01:12:15 -0800

    Added periods to error messages.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/CreateTrigger/components/Action/utils/validate.js
+++ public/pages/CreateTrigger/components/Action/utils/validate.js
@@ -10,10 +10,10 @@
  */
 
 export const validateDestination = (destinations) => (value) => {
-  if (!value) return 'Required';
+  if (!value) return 'Required.';
   // In case existing destination doesn't exist in list , invalidate the field
   const destinationMatches = destinations.filter((destination) => destination.value === value);
   if (destinationMatches.length === 0) {
-    return 'Required';
+    return 'Required.';
   }
 };
