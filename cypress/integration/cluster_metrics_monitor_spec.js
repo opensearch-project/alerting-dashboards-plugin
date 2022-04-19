6593f1139121c6d9e403043079e6ded76a775fe1
Mohammad Qureshi <47198598+qreshi@users.noreply.github.com>
2022-04-18 20:58:56 -0700

    Remove Destination related Cypress tests
    Signed-off-by: Mohammad Qureshi <47198598+qreshi@users.noreply.github.com>

--- cypress/integration/cluster_metrics_monitor_spec.js
+++ cypress/integration/cluster_metrics_monitor_spec.js
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import sampleDestination from '../fixtures/sample_destination_custom_webhook';
 import sampleClusterMetricsMonitor from '../fixtures/sample_cluster_metrics_monitor.json';
 import { INDEX, PLUGIN_NAME } from '../../cypress/support/constants';
 
