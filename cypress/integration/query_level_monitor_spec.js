5636c8e0ed2ad8fa7a9b5bb77895acd6645b7aff
AWSHurneyt <hurneyt@amazon.com>
2022-06-09 14:18:33 -0700

    Implemented a fix for issue 258 which was allowing the UX to define more than 1 index for document level monitors. (#259)
    * Implemented a fix for issue 258 which was allowing the UX to define more than 1 index for document level monitors.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Added command to add sample flight data.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored cypress test to confirm the monitor was updated successfully.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Implemented cypress tests to confirm query and bucket monitors can be created with multiple indices following the bug fix in PR 259.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored cypress tests specific to PR 259 to create their own indices instead of using sample data.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored the way cypress tests, specific to PR 259, create their test data.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- cypress/integration/query_level_monitor_spec.js
+++ cypress/integration/query_level_monitor_spec.js
@@ -19,6 +19,9 @@ const SAMPLE_TRIGGER = 'sample_trigger';
 const SAMPLE_ACTION = 'sample_action';
 const SAMPLE_DESTINATION = 'sample_destination';
 
+const TESTING_INDEX_A = 'query-level-monitor-test-index-a';
+const TESTING_INDEX_B = 'query-level-monitor-test-index-b';
+
 const addVisualQueryLevelTrigger = (
   triggerName,
   triggerIndex,
@@ -67,6 +70,13 @@ const addVisualQueryLevelTrigger = (
 };
 
 describe('Query-Level Monitors', () => {
+  before(() => {
+    // Load sample data
+    cy.loadSampleEcommerceData();
+    cy.insertDocumentToIndex(TESTING_INDEX_A, undefined, { message: 'This is a test.' });
+    cy.insertDocumentToIndex(TESTING_INDEX_B, undefined, { message: 'This is a test.' });
+  });
+
   beforeEach(() => {
     // Set welcome screen tracking to false
     localStorage.setItem('home:welcome:show', 'false');
@@ -145,20 +155,21 @@ describe('Query-Level Monitors', () => {
   });
 
   describe('can be updated', () => {
-    before(() => {
+    beforeEach(() => {
       cy.deleteAllMonitors();
       cy.createMonitor(sampleQueryLevelMonitor);
+      cy.reload();
     });
 
     it('by changing the name', () => {
       // Confirm we can see the created monitor in the list
-      cy.contains(SAMPLE_MONITOR);
+      cy.contains(SAMPLE_MONITOR, { timeout: 20000 });
 
       // Select the existing monitor
-      cy.get('a').contains(SAMPLE_MONITOR).click({ force: true });
+      cy.get('a').contains(SAMPLE_MONITOR, { timeout: 20000 }).click();
 
       // Click Edit button
-      cy.contains('Edit').click({ force: true });
+      cy.contains('Edit', { timeout: 20000 }).click({ force: true });
 
       // Wait for input to load and then type in the new monitor name
       cy.get('input[name="name"]')
@@ -177,6 +188,40 @@ describe('Query-Level Monitors', () => {
 
       // Confirm we can see the updated monitor in the list
       cy.contains(UPDATED_MONITOR);
+    });
+
+    it('to have multiple indices', () => {
+      // Confirm we can see the created monitor in the list
+      cy.contains(SAMPLE_MONITOR, { timeout: 20000 });
+
+      // Select the existing monitor
+      cy.get('a').contains(SAMPLE_MONITOR, { timeout: 20000 }).click({ force: true });
+
+      // Click Edit button
+      cy.contains('Edit', { timeout: 20000 }).click({ force: true });
+
+      // Click on the Index field and type in multiple index names to replicate the bug
+      cy.get('#index')
+        .click({ force: true })
+        .type(`${TESTING_INDEX_A}{enter}${TESTING_INDEX_B}{enter}`, {
+          force: true,
+        })
+        .trigger('blur', { force: true });
+
+      // Confirm Index field only contains the expected text
+      cy.get('[data-test-subj="indicesComboBox"]').contains('*', { timeout: 20000 });
+      cy.get('[data-test-subj="indicesComboBox"]').contains(TESTING_INDEX_A, {
+        timeout: 20000,
+      });
+      cy.get('[data-test-subj="indicesComboBox"]').contains(TESTING_INDEX_B, {
+        timeout: 20000,
+      });
+
+      // Click the update button
+      cy.get('button').contains('Update').last().click();
+
+      // Confirm we're on the Monitor Details page by searching for the History element
+      cy.contains('History', { timeout: 20000 });
     });
   });
 
@@ -235,7 +280,6 @@ describe('Query-Level Monitors', () => {
   describe('can have triggers', () => {
     before(() => {
       cy.deleteAllMonitors();
-      cy.loadSampleEcommerceData();
       cy.createMonitor(sampleQueryLevelMonitor);
     });
 
@@ -313,10 +357,11 @@ describe('Query-Level Monitors', () => {
       });
     });
   });
-  
+
   describe('schedule component displays as intended', () => {
     before(() => {
       cy.deleteAllMonitors();
+      cy.reload();
     });
 
     it('for an interval schedule', () => {
@@ -377,5 +422,7 @@ describe('Query-Level Monitors', () => {
 
     // Delete sample data
     cy.deleteIndexByName(`${INDEX.SAMPLE_DATA_ECOMMERCE}`);
+    cy.deleteIndexByName(TESTING_INDEX_A);
+    cy.deleteIndexByName(TESTING_INDEX_B);
   });
 });
--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -8,6 +8,7 @@ export const API_ROUTE_PREFIX = '/_plugins/_alerting';
 export const INDEX = {
   OPENSEARCH_ALERTING_CONFIG: '.opendistro-alerting-config',
   SAMPLE_DATA_ECOMMERCE: 'opensearch_dashboards_sample_data_ecommerce',
+  SAMPLE_DATA_FLIGHTS: 'opensearch_dashboards_sample_data_flights',
 };
 
 export const API = {
