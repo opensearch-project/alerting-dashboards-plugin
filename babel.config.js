260166d3067b8566dbf931febd2b5295e77ee5fe
Ashish Agrawal <ashisagr@amazon.com>
2022-10-04 15:06:41 -0700

    Revert "bumped package version (#339)"
    This reverts commit 2755366d2b659d0015d904cf35aca30f032ebab1.
    
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,5 @@ module.exports = {
   plugins: [
     require('@babel/plugin-proposal-class-properties'),
     require('@babel/plugin-proposal-object-rest-spread'),
-    require('@babel/plugin-transform-modules-commonjs'),
   ],
 };
