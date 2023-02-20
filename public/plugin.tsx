b186e49cb78e379c0ce09e1c692744992d813356
David Sinclair <dsincla@rei.com>
2023-02-19 17:31:31 -0800

    initial context menus and setup

---
+++ public/plugin.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { PLUGIN_NAME } from '../utils/constants';
+import { Plugin } from '../../../src/core/public';
+import { ACTION_ALERTING } from './actions/alerting_dashboard_action';
+import { CONTEXT_MENU_TRIGGER } from '../../../src/plugins/embeddable/public';
+import { getActions } from './utils/contextMenu';
+
+declare module '../../../src/plugins/ui_actions/public' {
+  export interface ActionContextMapping {
+    [ACTION_ALERTING]: {};
+  }
+}
+
+export class AlertingPlugin implements Plugin {
+  public setup(core, plugins) {
+    core.application.register({
+      id: PLUGIN_NAME,
+      title: 'Alerting',
+      description: 'OpenSearch Dashboards Alerting Plugin',
+      category: {
+        id: 'opensearch',
+        label: 'OpenSearch Plugins',
+        order: 2000,
+      },
+      order: 4000,
+      mount: async (params) => {
+        const { renderApp } = await import('./app');
+        const [coreStart] = await core.getStartServices();
+        return renderApp(coreStart, params);
+      },
+    });
+
+    // Create context menu actions. Pass core, to access service for flyouts.
+    const actions = getActions({ core, plugins });
+
+    // Add actions to uiActions
+    actions.forEach((action) => {
+      plugins.uiActions.addTriggerAction(CONTEXT_MENU_TRIGGER, action);
+    });
+  }
+
+  public start() {}
+
+  public stop() {}
+}
---
+++ public/utils/contextMenu.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { EuiIcon, EuiFlexItem, EuiFlexGroup } from '@elastic/eui';
+import { i18n } from '@osd/i18n';
+import { EuiIconType } from '@elastic/eui/src/components/icon/icon';
+import { toMountPoint } from '../../../../src/plugins/opensearch_dashboards_react/public';
+import { createAlertingAction } from '../actions/alerting_dashboard_action';
+import { Action } from '../../../../src/plugins/ui_actions/public';
+
+// This is used to create all actions in the same context menu
+const grouping: Action['grouping'] = [
+  {
+    id: 'alerting-dashboard-context-menu',
+    getDisplayName: () => 'Alerting',
+    getIconType: () => 'bell',
+  },
+];
+
+const DocumentationTitle = () => (
+  <EuiFlexGroup>
+    <EuiFlexItem>
+      {i18n.translate('dashboard.actions.alertingMenuItem.documentation.displayName', {
+        defaultMessage: 'Documentation',
+      })}
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiIcon type="popout" />
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
+
+export const getActions = ({ core, plugins }) =>
+  [
+    {
+      grouping,
+      id: 'createAlertingMonitor',
+      title: i18n.translate(
+        'dashboard.actions.alertingMenuItem.createAlertingMonitor.displayName',
+        {
+          defaultMessage: 'Add alerting monitor',
+        }
+      ),
+      icon: 'plusInCircle' as EuiIconType,
+      order: 100,
+      onClick: async ({ embeddable }) => {
+        const services = await core.getStartServices();
+        const openFlyout = services[0].overlays.openFlyout;
+        const overlay = openFlyout(toMountPoint(<div />), {
+          size: 'l',
+          className: 'create-alerting-monitor__flyout',
+        });
+      },
+    },
+    {
+      grouping,
+      id: 'manageMonitors',
+      title: i18n.translate('dashboard.actions.alertingMenuItem.manageMonitors.displayName', {
+        defaultMessage: 'Associated monitors',
+      }),
+      icon: 'gear' as EuiIconType,
+      order: 99,
+      onClick: async ({ embeddable }) => {
+        const services = await core.getStartServices();
+        const openFlyout = services[0].overlays.openFlyout;
+        const overlay = openFlyout(toMountPoint(<div />), { size: 'm' });
+      },
+    },
+    {
+      id: 'documentation',
+      title: <DocumentationTitle />,
+      icon: 'documentation' as EuiIconType,
+      order: 98,
+      onClick: () => {
+        window.open(
+          'https://opensearch.org/docs/latest/monitoring-plugins/alerting/index/',
+          '_blank'
+        );
+      },
+    },
+  ].map((options) => createAlertingAction({ ...options, grouping }));
