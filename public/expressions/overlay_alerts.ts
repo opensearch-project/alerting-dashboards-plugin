ae044a3440f8a04a6512146466ee5f39200c6efb
Ashish Agrawal <ashisagr@amazon.com>
2023-05-25 17:02:33 -0700

    add tests
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- public/expressions/overlay_alerts.ts
+++ public/expressions/overlay_alerts.ts
@@ -35,13 +35,8 @@ import {
   ExprVisLayers,
 } from '../../../../src/plugins/expressions/public';
 import { TimeRange, calculateBounds } from '../../../../src/plugins/data/common';
-import { getClient } from '../services';
-import {
-  VisLayers,
-  VisLayerTypes,
-  VisLayerError,
-  PointInTimeEventsVisLayer,
-} from '../../../../src/plugins/vis_augmenter/public';
+import { VisLayers } from '../../../../src/plugins/vis_augmenter/public';
+import { convertAlertsToLayer, getAlerts, getMonitorName } from './helpers';
 
 type Input = ExprVisLayers;
 type Output = Promise<ExprVisLayers>;
@@ -59,73 +54,6 @@ export type OverlayAlertsExpressionFunctionDefinition = ExpressionFunctionDefini
   Output
 >;
 
-const getAlerts = async (
-  monitorId: string,
-  startTime: number,
-  endTime: number
-): Promise<string> => {
-  const params = {
-    size: 1000,
-    sortField: 'start_time',
-    sortDirection: 'asc',
-    monitorIds: [monitorId],
-  };
-
-  const resp = await getClient().get('/api/alerting/alerts', { query: params });
-
-  if (resp.ok) {
-    const { alerts } = resp;
-
-    // added filter for monitor id since there is a bug in the backend for the alerts api
-    alerts
-      .filter((alert) => alert.start_time >= startTime && alert.start_time <= endTime)
-      .filter((alert) => alert.monitorId == monitorId);
-    return alerts;
-  } else {
-    console.log('error getting alerts:', resp);
-  }
-  return '';
-};
-
-const getMonitorName = async (monitorId: string): Promise<string> => {
-  const resp = await getClient().get('/api/alerting/monitors/' + monitorId);
-  if (resp.ok) {
-    return resp.resp.name as string;
-  } else if (resp.resp === '[alerting_exception] Monitor not found.') {
-    return 'RESOURCE_DELETED'
-  } else if (resp.resp.includes('Do not have permissions to resource') || resp.resp.includes('security_exception')) {
-    return 'FETCH_FAILURE'
-  }
-  return 'error loading monitor';
-};
-
-const convertAlertsToLayer = (
-  alerts: string,
-  monitorId: string,
-  monitorName: string,
-  error?: VisLayerError,
-): PointInTimeEventsVisLayer => {
-  const events = alerts.map((alert) => {
-    return {
-      timestamp: alert.start_time + (alert.end_time - alert.start_time) / 2,
-      metadata: {
-        pluginResourceId: monitorId,
-      },
-    };
-  });
-  return {
-    originPlugin: 'Alerting',
-    events,
-    pluginResource: {
-      type: 'Alerting Monitors',
-      id: monitorId,
-      name: monitorName,
-      urlPath: 'alerting#/monitors/' + monitorId,
-    },
-    type: VisLayerTypes.PointInTimeEvents,
-    error: error
-  } as PointInTimeEventsVisLayer;
-};
 
 export const overlayAlertsFunction = (): OverlayAlertsExpressionFunctionDefinition => ({
   name,
