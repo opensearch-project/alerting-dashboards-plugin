31827e699b914cdf7bafe1f34b5969416d9d0518
Ashish Agrawal <ashisagr@amazon.com>
2023-06-01 15:55:01 -0700

    Minor cleanup
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- public/expressions/helpers.ts
+++ public/expressions/helpers.ts
@@ -29,15 +29,15 @@ export const getAlerts = async (
     const filteredAlerts = resp.alerts.filter((alert) => alert.start_time >= startTime && alert.start_time <= endTime );
     return filteredAlerts;
   } else {
-    console.log('error getting alerts:', resp);
+    console.error('Error getting alerts to overlay:', resp);
   }
   return '';
 };
 
 export const getMonitorName = async (monitorId: string): Promise<string> => {
-  const resp = await getClient().get('/api/alerting/monitors/' + monitorId);
+  const resp = await getClient().get(`/api/alerting/monitors/${monitorId}`);
   if (resp.ok) {
-    return resp.resp.name as string;
+    return resp.resp.name;
   } else if (resp.resp === '[alerting_exception] Monitor not found.') {
     return VisLayerErrorTypes.RESOURCE_DELETED
   } else if (resp.resp.includes('Do not have permissions to resource') || resp.resp.includes('security_exception')) {
@@ -67,9 +67,9 @@ export const convertAlertsToLayer = (
       type: 'Alerting Monitors',
       id: monitorId,
       name: monitorName,
-      urlPath: 'alerting#/monitors/' + monitorId,
+      urlPath: `alerting#/monitors/${monitorId}`,
     },
     type: VisLayerTypes.PointInTimeEvents,
     error: error
-  } as PointInTimeEventsVisLayer;
+  };
 };
