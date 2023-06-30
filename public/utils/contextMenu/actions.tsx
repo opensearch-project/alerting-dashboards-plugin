5c5c57f925d72d18d7d28bd2028fc166e1615d31
Ashish Agrawal <ashisagr@amazon.com>
2023-06-30 12:00:11 -0700

    Context Menu Setup for Anywhere
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

---
+++ public/utils/contextMenu/actions.tsx
@@ -0,0 +1,120 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import _ from 'lodash';
+import React from 'react';
+import { i18n } from '@osd/i18n';
+import { EuiIconType } from '@elastic/eui/src/components/icon/icon';
+import { toMountPoint } from '../../../../../src/plugins/opensearch_dashboards_react/public';
+import { createAlertingAction } from '../../actions/alerting_dashboard_action';
+import { Action } from '../../../../../src/plugins/ui_actions/public';
+import DocumentationTitle from '../../components/FeatureAnywhereContextMenu/DocumentationTitle';
+import Container from '../../components/FeatureAnywhereContextMenu/Container';
+import { getOverlays } from '../../services';
+
+export const ALERTING_ACTION_CONTEXT = 'ALERTING_ACTION_CONTEXT';
+export const ALERTING_ACTION_CONTEXT_GROUP_ID = 'ALERTING_ACTION_CONTEXT_GROUP_ID';
+export const ALERTING_ACTION_ADD_ID = 'ALERTING_ACTION_ADD_ID';
+export const ALERTING_ACTION_ASSOCIATED_ID = 'ALERTING_ACTION_ASSOCIATED_ID';
+export const ALERTING_ACTION_DOC_ID = 'ALERTING_ACTION_DOC_ID';
+export const ALERTING_ACTION_AD = 'ALERTING_ACTION_AD';
+export const ALERTING_TRIGGER_AD_ID = 'ALERTING_TRIGGER_AD_ID';
+
+declare module '../../../../../src/plugins/ui_actions/public' {
+  export interface ActionContextMapping {
+    [ALERTING_ACTION_CONTEXT]: {};
+    [ALERTING_ACTION_AD]: {};
+  }
+}
+
+export const openContainerInFlyout = async ({
+  defaultFlyoutMode,
+  embeddable,
+  detectorId,
+}: {
+  defaultFlyoutMode: string;
+  embeddable: any;
+  detectorId?: string;
+}) => {
+  const clonedEmbeddable = await _.cloneDeep(embeddable);
+  const overlayService = getOverlays();
+  const openFlyout = overlayService.openFlyout;
+  const overlay = openFlyout(
+    toMountPoint(
+      <Container
+        {...{
+          defaultFlyoutMode,
+          embeddable: clonedEmbeddable,
+          closeFlyout: () => overlay.close(),
+          detectorId,
+        }}
+      />
+    ),
+    { size: 'm', className: 'context-menu__flyout' }
+  );
+};
+
+// This is used to create all actions in the same context menu
+const grouping: Action['grouping'] = [
+  {
+    id: ALERTING_ACTION_CONTEXT_GROUP_ID,
+    getDisplayName: () => 'Alerting',
+    getIconType: () => 'bell',
+    category: 'vis_augmenter',
+    order: 30,
+  },
+];
+
+export const getActions = () =>
+  [
+    {
+      id: ALERTING_ACTION_ADD_ID,
+      title: i18n.translate('dashboard.actions.alertingMenuItem.addAlertingMonitor.displayName', {
+        defaultMessage: 'Add alerting monitor',
+      }),
+      icon: 'plusInCircle' as EuiIconType,
+      order: 100,
+      onExecute: ({ embeddable }) =>
+        openContainerInFlyout({ embeddable, defaultFlyoutMode: 'create' }),
+    },
+    {
+      id: ALERTING_ACTION_ASSOCIATED_ID,
+      title: i18n.translate('dashboard.actions.alertingMenuItem.associatedMonitors.displayName', {
+        defaultMessage: 'Associated monitors',
+      }),
+      icon: 'kqlSelector' as EuiIconType,
+      order: 99,
+      onExecute: ({ embeddable }) =>
+        openContainerInFlyout({ embeddable, defaultFlyoutMode: 'associated' }),
+    },
+    {
+      id: ALERTING_ACTION_DOC_ID,
+      title: <DocumentationTitle />,
+      icon: 'documentation' as EuiIconType,
+      order: 98,
+      onExecute: () => {
+        window.open(
+          'https://opensearch.org/docs/latest/observing-your-data/alerting/dashboards-alerting/',
+          '_blank'
+        );
+      },
+    },
+  ].map((options) => createAlertingAction({ ...options, grouping, type: ALERTING_ACTION_CONTEXT }));
+
+export const getAdAction = () =>
+  createAlertingAction({
+    type: ALERTING_ACTION_AD,
+    grouping: [],
+    id: ALERTING_TRIGGER_AD_ID,
+    title: ALERTING_TRIGGER_AD_ID,
+    icon: '' as EuiIconType,
+    order: 1,
+    onExecute: ({ embeddable, detectorId }) =>
+      openContainerInFlyout({
+        embeddable,
+        detectorId,
+        defaultFlyoutMode: 'adMonitor',
+      }),
+  });
--- public/utils/savedObjectHelper.tsx
+++ public/utils/savedObjectHelper.tsx
@@ -12,7 +12,56 @@ import {
   VisLayerExpressionFn,
   VisLayerTypes,
 } from '../../../../src/plugins/vis_augmenter/public';
-import { getSavedAugmentVisLoader, getUISettings } from '../services';
+import { getSavedAugmentVisLoader, getUISettings, getNotifications } from '../services';
+import {
+  PLUGIN_AUGMENTATION_ENABLE_SETTING,
+  PLUGIN_AUGMENTATION_MAX_OBJECTS_SETTING,
+} from './constants';
+
+export const validateAssociationIsAllow = async (visId, sendDangerToast = false) => {
+  const uiSettings = getUISettings();
+  const notifications = getNotifications();
+
+  const isAugmentationEnabled = uiSettings.get(PLUGIN_AUGMENTATION_ENABLE_SETTING);
+  if (!isAugmentationEnabled) {
+    if (sendDangerToast) {
+      notifications.toasts.addDanger(
+        'Visualization augmentation is disabled, please enable visualization:enablePluginAugmentation.'
+      );
+    }
+    return false;
+  }
+
+  const maxAssociatedCount = uiSettings.get(PLUGIN_AUGMENTATION_MAX_OBJECTS_SETTING);
+  const currentAssociatedCount = await getCountOfAssociatedObjects(visId);
+  if (maxAssociatedCount <= currentAssociatedCount) {
+    if (sendDangerToast) {
+      notifications.toasts.addDanger(
+        `Cannot create the monitor and associate it to the visualization due to the limit of the max
+            amount of associated plugin resources (${maxAssociatedCount}) with
+            ${currentAssociatedCount} associated to the visualization`
+      );
+    }
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const getCountOfAssociatedObjects = async (visId) => {
+  const loader = getSavedAugmentVisLoader();
+
+  return await loader.findAll().then(async (resp) => {
+    if (resp !== undefined) {
+      const savedAugmentObjects = get(resp, 'hits', []);
+      // gets all the saved object for this visualization
+      const savedObjectsForThisVisualization = savedAugmentObjects.filter(
+        (savedObj) => get(savedObj, 'visId', '') === visId
+      );
+      return savedObjectsForThisVisualization.length;
+    }
+  });
+};
 
 export const createSavedObjectAssociation = async (monitorId, embeddable) => {
   const loader = getSavedAugmentVisLoader();
@@ -28,46 +77,46 @@ export const createSavedObjectAssociation = async (monitorId, embeddable) => {
     type: VisLayerTypes.PointInTimeEvents,
     name: 'overlay_alerts',
     args: {
-      monitorId: monitorId,
+      monitorId,
     },
   };
   const savedObjectToCreate: ISavedAugmentVis = {
     title: embeddable.vis.title,
     description: 'Association to Alerting monitor',
     originPlugin: 'alertingDashboards',
-    pluginResource: pluginResource,
+    pluginResource,
     visName: embeddable.vis.title,
     visId: embeddable.vis.id,
     visLayerExpressionFn: fn,
   };
 
-  let newSavedObj = await createAugmentVisSavedObject(savedObjectToCreate, loader, uiSettings);
+  const newSavedObj = await createAugmentVisSavedObject(savedObjectToCreate, loader, uiSettings);
   return newSavedObj.save({});
 };
 
 export const getAlertingAugmentVisSavedObjs = async (
-  visId: string,
+  visId: string
 ): Promise<ISavedAugmentVis[]> => {
   const loader = getSavedAugmentVisLoader();
   const uiSettings = getUISettings();
   const savedObjects = await getAugmentVisSavedObjs(visId, loader, uiSettings);
-  return savedObjects.filter((savedObject) => savedObject.visLayerExpressionFn.name === 'overlay_alerts');
+  return savedObjects.filter(
+    (savedObject) => savedObject.visLayerExpressionFn.name === 'overlay_alerts'
+  );
 };
 
-export const getAssociatedMonitorIds = async (
-  visId: string,
-): Promise<String[]> => {
+export const getAssociatedMonitorIds = async (visId: string): Promise<string[]> => {
   const savedObjects = await getAlertingAugmentVisSavedObjs(visId);
   const associatedMonitorIds: string[] = [];
   for (const associatedObject of savedObjects) {
-    associatedMonitorIds.push(associatedObject.pluginResource.id)
+    associatedMonitorIds.push(associatedObject.pluginResource.id);
   }
   return associatedMonitorIds;
 };
 
 export const deleteAlertingAugmentVisSavedObj = async (
   visId: string,
-  monitorId: string,
+  monitorId: string
 ): Promise<void> => {
   const savedObjectLoader = getSavedAugmentVisLoader();
   await savedObjectLoader.findAll().then(async (resp) => {
@@ -85,12 +134,17 @@ export const deleteAlertingAugmentVisSavedObj = async (
       );
       const savedObjectToUnlinkId = get(savedAugmentToUnlink, 'id', '');
       if (savedObjectToUnlinkId === '') {
-        throw new Error('Failed to retrieve the saved object that associates the visualization and the Alerting monitor.');
+        throw new Error(
+          'Failed to retrieve the saved object that associates the visualization and the Alerting monitor.'
+        );
       }
       try {
         await savedObjectLoader.delete(savedObjectToUnlinkId);
       } catch (e) {
-        throw new Error('Failed to delete the saved object that associates the visualization and the Alerting monitor. Reason:' + e.message);
+        throw new Error(
+          'Failed to delete the saved object that associates the visualization and the Alerting monitor. Reason:' +
+          e.message
+        );
       }
     }
   });
