72d5195160e14f028e8069be10293366a53bbe92
Ashish Agrawal <ashisagr@amazon.com>
2023-06-30 14:44:03 -0700

    updateTests
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- public/components/FeatureAnywhereContextMenu/Container/Container.test.js
+++ public/components/FeatureAnywhereContextMenu/Container/Container.test.js
@@ -4,19 +4,23 @@
  */
 
 import React from 'react';
+import { uiSettingsServiceMock } from '../../../../../../src/core/public/mocks';
 import { shallow } from 'enzyme';
 import Container from './Container';
+import { setUISettings } from '../../../services';
 
 describe('Container', () => {
+  const uiSettingsMock = uiSettingsServiceMock.createStartContract();
+  setUISettings(uiSettingsMock);
   test('renders add', () => {
     const wrapper = shallow(
-      <Container {...{ startingPanel: 'add', embeddable: { getTitle: () => '' } }} />
+      <Container {...{ startingPanel: 'add', embeddable: { vis: { title: '' } } }} />
     );
     expect(wrapper).toMatchSnapshot();
   });
   test('renders associated', () => {
     const wrapper = shallow(
-      <Container {...{ startingPanel: 'associated', embeddable: { getTitle: () => '' } }} />
+      <Container {...{ startingPanel: 'associated', embeddable: { vis: { title: '' } } }} />
     );
     expect(wrapper).toMatchSnapshot();
   });
