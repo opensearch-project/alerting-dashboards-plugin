a68029b467c652bb25d26729b820a3532bfa5025
Ashish Agrawal <ashisagr@amazon.com>
2023-07-06 09:22:02 -0700

    Context Menu Setup for Anywhere (#589)
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- public/plugin.tsx
+++ public/plugin.tsx
@@ -4,12 +4,27 @@
  */
 
 import { PLUGIN_NAME } from '../utils/constants';
-import { Plugin, CoreSetup, CoreStart } from '../../../src/core/public';
+import {
+  Plugin,
+  CoreSetup,
+  CoreStart,
+} from '../../../src/core/public';
+import { ACTION_ALERTING } from './actions/alerting_dashboard_action';
+import { CONTEXT_MENU_TRIGGER, EmbeddableStart } from '../../../src/plugins/embeddable/public';
+import { getActions, getAdAction } from './utils/contextMenu/actions';
+import { alertingTriggerAd } from './utils/contextMenu/triggers';
 import { ExpressionsSetup } from '../../../src/plugins/expressions/public';
 import { UiActionsSetup } from '../../../src/plugins/ui_actions/public';
-import { setSavedAugmentVisLoader, setUISettings, setClient } from './services';
-import { VisAugmenterStart } from '../../../src/plugins/vis_augmenter/public';
 import { overlayAlertsFunction } from './expressions/overlay_alerts';
+import { setClient, setEmbeddable, setNotifications, setOverlays, setSavedAugmentVisLoader, setUISettings, setQueryService } from './services';
+import { VisAugmenterStart } from '../../../src/plugins/vis_augmenter/public';
+import { DataPublicPluginStart } from '../../../src/plugins/data/public';
+
+declare module '../../../src/plugins/ui_actions/public' {
+  export interface ActionContextMapping {
+    [ACTION_ALERTING]: {};
+  }
+}
 
 export interface AlertingSetup {}
 
@@ -22,10 +37,12 @@ export interface AlertingSetupDeps {
 
 export interface AlertingStartDeps {
   visAugmenter: VisAugmenterStart;
+  embeddable: EmbeddableStart;
+  data: DataPublicPluginStart;
 }
 
 export class AlertingPlugin implements Plugin<AlertingSetup, AlertingStart, AlertingSetupDeps, AlertingStartDeps> {
-  public setup(core: CoreSetup<AlertingSetupDeps, AlertingStart>, { expressions, uiActions }: AlertingSetupDeps): AlertingSetup {
+  public setup(core: CoreSetup<AlertingStartDeps, AlertingStart>, { expressions, uiActions }: AlertingSetupDeps): AlertingSetup {
     core.application.register({
       id: PLUGIN_NAME,
       title: 'Alerting',
@@ -43,21 +60,39 @@ export class AlertingPlugin implements Plugin<AlertingSetup, AlertingStart, Aler
       },
     });
 
+    setUISettings(core.uiSettings);
+
     // Set the HTTP client so it can be pulled into expression fns to make
     // direct server-side calls
     setClient(core.http);
 
-    // Register the UI Settings for the helper CRUD Saved Augement Loader functions
-    setUISettings(core.uiSettings);
-
     // registers the expression function used to render anomalies on an Augmented Visualization
     expressions.registerFunction(overlayAlertsFunction());
 
+    // Create context menu actions. Pass core, to access service for flyouts.
+    const actions = getActions();
+
+    // Add actions to uiActions
+    actions.forEach((action) => {
+      uiActions.addTriggerAction(CONTEXT_MENU_TRIGGER, action);
+    });
+
+    // Create trigger for other plugins to open flyout. Can be used by other plugins like this:
+    const adAction = getAdAction();
+    uiActions.registerTrigger(alertingTriggerAd);
+    uiActions.addTriggerAction(alertingTriggerAd.id, adAction);
+
+    return;
+  }
+
+  public start(core: CoreStart, { visAugmenter, embeddable, data }: AlertingStartDeps): AlertingStart {
+    setEmbeddable(embeddable);
+    setOverlays(core.overlays);
+    setQueryService(data.query);
+    setSavedAugmentVisLoader(visAugmenter.savedAugmentVisLoader);
+    setNotifications(core.notifications);
     return {};
   }
 
-  public start(core: CoreStart, { visAugmenter }: AlertingStartDeps): AlertingStart {
-    setSavedAugmentVisLoader(visAugmenter.savedAugmentVisLoader);
-    return {};
-  }
+  public stop() {}
 }
--- public/services/services.ts
+++ public/services/services.ts
@@ -6,8 +6,10 @@
 import { createContext } from 'react';
 import { BrowserServices } from '../models/interfaces';
 import { createGetterSetter } from '../../../../src/plugins/opensearch_dashboards_utils/public';
-import { CoreStart, IUiSettingsClient } from '../../../../src/core/public';
+import { CoreStart, IUiSettingsClient, NotificationsStart, OverlayStart } from '../../../../src/core/public';
 import { SavedObjectLoaderAugmentVis } from '../../../../src/plugins/vis_augmenter/public';
+import { DataPublicPluginStart } from '../../../../src/plugins/data/public';
+import { EmbeddableStart } from '../../../../src/plugins/embeddable/public';
 
 const ServicesContext = createContext<BrowserServices | null>(null);
 
@@ -15,10 +17,23 @@ const ServicesConsumer = ServicesContext.Consumer;
 
 export { ServicesContext, ServicesConsumer };
 
+export const [getClient, setClient] =
+  createGetterSetter<CoreStart['http']>('http');
+
 export const [getSavedAugmentVisLoader, setSavedAugmentVisLoader] = createGetterSetter<
   SavedObjectLoaderAugmentVis
   >('savedAugmentVisLoader');
 
 export const [getUISettings, setUISettings] = createGetterSetter<IUiSettingsClient>('UISettings');
-export const [getClient, setClient] =
-  createGetterSetter<CoreStart['http']>('http');
+
+export const [getEmbeddable, setEmbeddable] = createGetterSetter<EmbeddableStart>('embeddable');
+
+export const [getOverlays, setOverlays] =
+  createGetterSetter<OverlayStart>('Overlays');
+
+export const [getQueryService, setQueryService] = createGetterSetter<
+  DataPublicPluginStart['query']
+  >('Query');
+
+export const [getNotifications, setNotifications] =
+  createGetterSetter<NotificationsStart>('Notifications');
