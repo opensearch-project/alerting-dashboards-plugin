043881b52dfa8749016a141c38c695ee6f7d76aa
AWSHurneyt <hurneyt@amazon.com>
2023-07-13 16:08:31 -0700

    Fix URI string generation logic. (#639)
    * Fix URI string generation logic.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated test.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/CreateMonitor/containers/CreateMonitor/utils/formikToMonitor.js
+++ public/pages/CreateMonitor/containers/CreateMonitor/utils/formikToMonitor.js
@@ -157,13 +157,18 @@ export function formikToClusterMetricsInput(values) {
   if (_.isEmpty(apiType)) apiType = getApiType(_.get(values, 'uri'));
   let pathParams = _.get(values, 'uri.path_params', FORMIK_INITIAL_VALUES.uri.path_params);
   pathParams = _.trim(pathParams);
+  // Trim '/' characters from the beginning and end of the path
+  pathParams = pathParams?.replace(/^\/+|\/+$/g, '');
   const hasPathParams = !_.isEmpty(pathParams);
   const path = getApiPath(hasPathParams, apiType);
   let url = FORMIK_INITIAL_VALUES.uri.url;
   if (!_.isEmpty(apiType)) {
     url = URL_DEFAULT_PREFIX;
     if (!_.isEmpty(path)) url = url + '/' + path;
-    if (hasPathParams) url = url + '/' + pathParams + _.get(API_TYPES, `${apiType}.appendText`, '');
+    if (hasPathParams) {
+      pathParams = '/' + pathParams;
+      url = url + pathParams + _.get(API_TYPES, `${apiType}.appendText`, '');
+    }
   }
   return {
     uri: {
