afd532f1500192249f4ad00911a1d51e17a02639
AWSHurneyt <hurneyt@amazon.com>
2023-07-13 15:56:15 -0700

    Updated test.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/CreateMonitor/containers/CreateMonitor/utils/formikToMonitor.js
+++ public/pages/CreateMonitor/containers/CreateMonitor/utils/formikToMonitor.js
@@ -159,14 +159,16 @@ export function formikToClusterMetricsInput(values) {
   pathParams = _.trim(pathParams);
   // Trim '/' characters from the beginning and end of the path
   pathParams = pathParams?.replace(/^\/+|\/+$/g, '');
-  pathParams = '/' + pathParams;
   const hasPathParams = !_.isEmpty(pathParams);
   const path = getApiPath(hasPathParams, apiType);
   let url = FORMIK_INITIAL_VALUES.uri.url;
   if (!_.isEmpty(apiType)) {
     url = URL_DEFAULT_PREFIX;
     if (!_.isEmpty(path)) url = url + '/' + path;
-    if (hasPathParams) url = url + pathParams + _.get(API_TYPES, `${apiType}.appendText`, '');
+    if (hasPathParams) {
+      pathParams = '/' + pathParams;
+      url = url + pathParams + _.get(API_TYPES, `${apiType}.appendText`, '');
+    }
   }
   return {
     uri: {
