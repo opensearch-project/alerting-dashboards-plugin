3da8f333d45eceaab91b567a4e7433692eeb3558
AWSHurneyt <hurneyt@amazon.com>
2024-03-15 16:05:56 -0700

    [Backport 2.5] Removed "last updated by" sections from the UI. (#767) (#902)
    * Removed "last updated by" sections from the UI. (#767)
    
    * Removed "last updated by" section from the UI as the SearchMonitor API can no longer return that info.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated cypress workflow.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    (cherry picked from commit 1bea77f5af67faf294ce24f9239914c6a0c5c7a8)
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated snapshots.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed import.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed test.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed cypress test.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed cypress test.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- cypress/integration/cluster_metrics_monitor_spec.js
+++ cypress/integration/cluster_metrics_monitor_spec.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import sampleClusterMetricsMonitor from '../fixtures/sample_cluster_metrics_monitor.json';
+import sampleClusterMetricsMonitor from '../fixtures/sample_cluster_metrics_health_monitor.json';
 import { INDEX, PLUGIN_NAME } from '../../cypress/support/constants';
 
 const SAMPLE_CLUSTER_METRICS_HEALTH_MONITOR = 'sample_cluster_metrics_health_monitor';
@@ -101,8 +101,8 @@ describe('ClusterMetricsMonitor', () => {
       // Wait for the API types to load and then type in the Cluster Health API
       cy.get('[data-test-subj="clusterMetricsApiTypeComboBox"]').type('cluster health{enter}');
 
-      // Confirm the Query parameters field is present and described as "optional"
-      cy.contains('Query parameters - optional');
+      // Confirm the Path parameters field is present and described as "optional"
+      cy.contains('Path parameters - optional');
       cy.get('[data-test-subj="clusterMetricsParamsFieldText"]');
 
       // Press the 'Run for response' button
@@ -158,8 +158,8 @@ describe('ClusterMetricsMonitor', () => {
       // Wait for the API types to load and then type in the Cluster Health API
       cy.get('[data-test-subj="clusterMetricsApiTypeComboBox"]').type('nodes stats{enter}');
 
-      // Confirm the Query parameters field is not present
-      cy.contains('Query parameters').should('not.exist');
+      // Confirm the Path parameters field is not present
+      cy.contains('Path parameters').should('not.exist');
       cy.get('[data-test-subj="clusterMetricsParamsFieldText"]').should('not.exist');
 
       // Press the 'Run for response' button
@@ -200,7 +200,7 @@ describe('ClusterMetricsMonitor', () => {
     });
   });
 
-  describe('displays Query parameters field appropriately', () => {
+  describe('displays Path parameters field appropriately', () => {
     beforeEach(() => {
       cy.deleteAllMonitors();
       cy.reload();
@@ -222,9 +222,9 @@ describe('ClusterMetricsMonitor', () => {
       // Wait for the API types to load and then type in the Cluster Health API
       cy.get('[data-test-subj="clusterMetricsApiTypeComboBox"]').type('list snapshots{enter}');
 
-      // Confirm the Query parameters field is present and is not described as "optional"
-      cy.contains('Query parameters - optional').should('not.exist');
-      cy.contains('Query parameters');
+      // Confirm the Path parameters field is present and is not described as "optional"
+      cy.contains('Path parameters - optional').should('not.exist');
+      cy.contains('Path parameters');
       cy.get('[data-test-subj="clusterMetricsParamsFieldText"]');
     });
   });
