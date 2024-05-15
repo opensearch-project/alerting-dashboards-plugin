2cd238f71a85d95e0ccaabaff99022ad82bd4a25
Riya Saxena <riysaxen@amazon.com>
2024-05-15 16:58:02 -0700

    remve dev code
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- public/utils/MultiDataSourceContext.js
+++ public/utils/MultiDataSourceContext.js
@@ -3,7 +3,6 @@ import React, { createContext, useState } from 'react';
 const MultiDataSourceContext = createContext();
 
 export const MultiDataSourceContextProvider = ({ children, value }) => {
-  console.log('da in mdsCompo is ', value);
   const [dataSourceId, setDataSourceId] = useState(value.dataSourceId);
   return (
     <MultiDataSourceContext.Provider value={{ dataSourceId, setDataSourceId }}>
