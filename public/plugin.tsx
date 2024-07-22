4f3852edc80a1bd8d518a75004836d45b1f5b5b8
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-07-22 09:08:10 -0700

    Update public/plugin.tsx
    Co-authored-by: SuZhou-Joe <suzhou@amazon.com>
    Signed-off-by: Riya <69919272+riysaxen-amzn@users.noreply.github.com>

--- public/plugin.tsx
+++ public/plugin.tsx
@@ -92,7 +92,7 @@ export class AlertingPlugin implements Plugin<void, AlertingStart, AlertingSetup
         id: ALERTS_NAV_ID,
         title: 'Alerts',
         order: 9070,
-        category: DEFAULT_APP_CATEGORIES.configure,
+        category: DEFAULT_APP_CATEGORIES.detect,
         workspaceAvailability: WorkspaceAvailability.outsideWorkspace,
         mount: async (params: AppMountParameters) => {
           return mountWrapper(params, "/dashboard");
