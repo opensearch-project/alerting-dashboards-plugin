0abb49b5ebb0bf5eedbaf391ed1a9a0b969fb2b9
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-07-22 09:07:49 -0700

    Update public/plugin.tsx
    Co-authored-by: SuZhou-Joe <suzhou@amazon.com>
    Signed-off-by: Riya <69919272+riysaxen-amzn@users.noreply.github.com>

--- public/plugin.tsx
+++ public/plugin.tsx
@@ -114,7 +114,7 @@ export class AlertingPlugin implements Plugin<void, AlertingStart, AlertingSetup
         id: DESTINATIONS_NAV_ID,
         title: 'Destinations',
         order: 9070,
-        category: DEFAULT_APP_CATEGORIES.configure,
+        category: DEFAULT_APP_CATEGORIES.detect,
         workspaceAvailability: WorkspaceAvailability.outsideWorkspace,
         mount: async (params: AppMountParameters) => {
           return mountWrapper(params, "/destinations");
