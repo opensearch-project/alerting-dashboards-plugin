543fa155f19612136c813d55faa120a2115ceb13
AWSHurneyt <hurneyt@amazon.com>
2025-03-03 15:43:10 -0800

    Reconfigured cypress to support v12.17.4.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  env: {
+    opensearch_url: 'localhost:9200',
+    opensearch_dashboards: 'http://localhost:5601',
+    security_enabled: false,
+  },
+  fixturesFolder: '.cypress/fixtures',
+  screenshotsFolder: '.cypress/screenshots',
+  videosFolder: '.cypress/videos',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./.cypress/plugins/index.js')(on, config)
+    },
+    specPattern: '.cypress/integration/*.spec.js',
+    supportFile: '.cypress/support/index.js',
+  },
+})
